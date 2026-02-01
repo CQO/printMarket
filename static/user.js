de078bf5ca32553331bd03272ba37ea71d647310
PUGE <mail@puge.org>
2026-02-02 00:41:20 +0800

    save

---
+++ static/user.js
@@ -0,0 +1,7 @@
+window.userInfo = localStorage.getItem('user_info') ? JSON.parse(localStorage.getItem('user_info')) : null;
+function checkLogin() {
+    if (!window.userInfo) {
+        alert('当前操作需要登录!');
+        window.location.href = '/login.html';
+    }
+}
