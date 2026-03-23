96a1173665a875ad5435830e8edc89cb5240b578
PUGE <mail@puge.org>
2026-03-23 18:49:37 +0800

    save

--- static/admin-user.js
+++ static/admin-user.js
@@ -19,3 +19,42 @@ setTimeout(() => {
         checkLogin()
     }
 }, 100);
+
+// 保存CSV
+function fake_click(obj) {
+var ev = document.createEvent("MouseEvents");
+ev.initMouseEvent(
+    "click", true, false, window, 0, 0, 0, 0, 0
+    , false, false, false, false, 0, null
+    );
+obj.dispatchEvent(ev);
+}
+function download(name, data) {
+var urlObject = window.URL || window.webkitURL || window;
+
+var downloadData = new Blob([data]);
+
+var save_link = document.createElementNS("http://www.w3.org/1999/xhtml", "a")
+save_link.href = urlObject.createObjectURL(downloadData);
+save_link.download = name;
+fake_click(save_link);
+}
+
+function saveCSV (name, data) {
+let saveData = ''
+data.forEach(element => {
+    if (element) {
+    saveData += element.join(',')
+    saveData += '\r\n'
+    }
+});
+
+var urlObject = window.URL || window.webkitURL || window;
+
+var downloadData = new Blob(['\ufeff' + saveData], { type: 'text/csv,charset=UTF-8'});
+
+var save_link = document.createElementNS("http://www.w3.org/1999/xhtml", "a")
+save_link.href = urlObject.createObjectURL(downloadData);
+save_link.download = name;
+fake_click(save_link);
+}
